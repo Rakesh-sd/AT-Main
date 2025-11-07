43b8e0cfaa75e2dea71068687f7fe615fe9dd883
RabbuRomeo <156200260+Tech-Skilldunia@users.noreply.github.com>
2025-11-07 17:27:47 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 var createError = require("http-errors");
-var express = require("express");
-const express = require('express');
+const express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var bodyParser = require("body-parser");
-
 const fs = require("fs");
 const puppeteer = require("puppeteer");
 const { google } = require("googleapis");
 
-var app = express();
-var cookieParser = require("cookie-parser");
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
