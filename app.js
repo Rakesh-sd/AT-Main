5226961853c65b87de7dbb997dc284e2acf1786b
RabbuRomeo <156200260+Tech-Skilldunia@users.noreply.github.com>
2025-11-07 18:24:54 +0530

    Initial commit

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var bodyParser = require("body-parser");
 const fs = require("fs");
-const { google } = require("googleapis");
 
 var app = express();
 
