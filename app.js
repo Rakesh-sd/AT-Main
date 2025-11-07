7127b3b0bba53fc6091668cba7555ed9832a1007
RabbuRomeo <156200260+Tech-Skilldunia@users.noreply.github.com>
2025-11-07 17:25:41 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var createError = require("http-errors");
 var express = require("express");
+const express = require('express');
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var bodyParser = require("body-parser");
-const something = require('./config')
 
 const fs = require("fs");
 const puppeteer = require("puppeteer");
