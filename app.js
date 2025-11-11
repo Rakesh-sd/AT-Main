444193e334a02bfcb6985a9596e1f5155df97b5f
RabbuRomeo <156200260+Tech-Skilldunia@users.noreply.github.com>
2025-11-11 19:06:16 +0530

    Initial commit

--- app.js
+++ app.js
@@ -37,10 +37,6 @@ app.get("/aboutus", (req, res) => {
 //   res.render("pages/inner2");
 // });
 
-app.get("/course", (req, res) => {
-  res.render("pages/inner");
-});
-
 app.get("/catalog", (req, res) => {
   res.render("pages/catalog");
 });
@@ -55,6 +51,112 @@ app.get("/terms-&-conditions", (req, res) => {
 
 app.get("/refund-policy", (req, res) => {
   res.render("pages/refund");
+});
+
+// innerpages
+
+app.get("/course", (req, res) => {
+  res.render("innerpages/sample");
+});
+
+// CSE
+
+app.get("/machine-learning", (req, res) => {
+  res.render("./innerpages/machine-learning.ejs");
+});
+
+app.get("/artificial-intelligence", (req, res) => {
+  res.render("./innerpages/artificial-intelligence.ejs");
+});
+
+app.get("/full-stack-web-development", (req, res) => {
+  res.render("./innerpages/full-stack.ejs");
+});
+
+app.get("/data-science", (req, res) => {
+  res.render("./innerpages/data-science.ejs");
+});
+
+app.get("/cyber-security", (req, res) => {
+  res.render("./innerpages/cyber-security.ejs");
+});
+
+app.get("/azure-cloud", (req, res) => {
+  res.render("./innerpages/azure-cloud.ejs");
+});
+
+app.get("/augmented-virtual-reality", (req, res) => {
+  res.render("./innerpages/augmented-reality.ejs");
+});
+
+app.get("/android-app", (req, res) => {
+  res.render("./innerpages/android-app.ejs");
+});
+
+
+
+
+
+
+
+app.get("/robotics", (req, res) => {
+  res.render("./innerpages/robotics.ejs");
+});
+app.get("/cardesign", (req, res) => {
+  res.render("./innerpages/cardesign.ejs");
+});
+app.get("/hr", (req, res) => {
+  res.render("./innerpages/hr.ejs");
+});
+
+app.get("/business", (req, res) => {
+  res.render("./innerpages/business.ejs");
+});
+app.get("/stock", (req, res) => {
+  res.render("./innerpages/stock.ejs");
+});
+app.get("/marketing", (req, res) => {
+  res.render("./innerpages/marketing.ejs");
+});
+app.get("/finance", (req, res) => {
+  res.render("./innerpages/finance.ejs");
+});
+app.get("/nano", (req, res) => {
+  res.render("./innerpages/nano.ejs");
+});
+app.get("/genetic", (req, res) => {
+  res.render("./innerpages/genetic.ejs");
+});
+
+
+
+app.get("/iot", (req, res) => {
+  res.render("./innerpages/iot.ejs");
+});
+app.get("/embedded", (req, res) => {
+  res.render("./innerpages/embedded.ejs");
+});
+app.get("/autocad", (req, res) => {
+  res.render("./innerpages/autocad.ejs");
+});
+app.get("/icengine", (req, res) => {
+  res.render("./innerpages/icengine.ejs");
+});
+app.get("/hybrid", (req, res) => {
+  res.render("./innerpages/hybrid.ejs");
+});
+app.get("/digital", (req, res) => {
+  res.render("./innerpages/digital.ejs");
+});
+
+app.get("/medicalcoding", (req, res) => {
+  res.render("./innerpages/medicalcoding.ejs");
+});
+app.get("/pharmacovigilance", (req, res) => {
+  res.render("./innerpages/pharmacovigilance.ejs");
+});
+app.get("/clinicalsas", (req, res) => {
+  res.render("./innerpages/clinicalsas.ejs");
 });
 
 // error handling
